aa4fda7438c9662bac571b05dcd8d44972a089ce
onlypatric <patric.personal99@gmail.com>
2022-12-06 09:18:43 +0100

    edited to 2 values

--- README.md
+++ README.md
@@ -5,13 +5,13 @@ Version: 1.0 06/12/2022 (dd/mm/yyyy)
 
 ## Project description
 
-1)generare un numero (int) compreso tra 1 e 10 (estremi inclusi)
+1)generare 2 numeri (int) compresi tra 1 e 10 (estremi inclusi)
 2)scelta controllata (0 oppure 1)
 3) 
     se 0:
-        stampa tot * in orizzontale quanto il voto 
+        stampa tot * in orizzontale quanto i voti 
     se 1:
-        stampa tot # in verticale quanto il voto 
+        stampa tot # in verticale quanto i voti
 
 ## Folder Structure
 
@@ -20,6 +20,24 @@ The workspace contains one folder by default, where:
 - `src`: the folder to maintain sources
 
 Meanwhile, the compiled output files will be generated in the `bin` folder by default.
+
+```
+Tree directory 
+.
+├── bin
+│   ├── App.class
+│   └── pkg
+│       ├── build
+│       └── util
+│           └── RUtil.class
+├── README.md
+└── src
+    ├── App.java
+    └── pkg
+        ├── build
+        └── util
+            ├── RUtil.class
+            └── RUtil.java```
 
 > If you want to customize the folder structure, open `.vscode/settings.json` and update the related settings there.
 
--- src/App.java
+++ src/App.java
@@ -10,22 +10,26 @@ public class App {
 
         RUtil r = new RUtil();
         Scanner sc = new Scanner(System.in);
-        int voto = r.randInt(1, 10),select=2;
+        int voto0 = r.randInt(1, 10),select=2;
+        int voto1 = r.randInt(1, 10);
         do {
             try {
                 System.out.print("Inserire un numero\n0 - stampa \"*\" in orizzontale\n1 - stampa \"#\" in verticale\nSeleziona: ");
-                // si usa parseInt (se si mette solo sc.nextInt potrà funzionare, ma se da "Exception" Scanner si "autochiude" e non funziona più)
+                // si usa parseInt (se si mette solo Scanner.nextInt potrà funzionare, ma se da "Exception" Scanner si "autochiude" e non funziona più)
                 select=Integer.parseInt(sc.nextLine());
             } catch (Exception e) {
                 System.out.println("Errore durante l'inserimento, probabilmente il numero inserito è in un formato incorretto");
             }
         } while (select<0||select>1);
-        String selectionString;
+        String selectionString="";
         if(select==0){
-            selectionString="*";
+            selectionString="*".repeat(voto0)+"\n"+"*".repeat(voto1);
         }else{
-            selectionString="#\n";
+            for (int i = Math.max(voto0,voto1); i>=0; i--) {
+                selectionString+="#\n";
+            }
+            selectionString+="# #\n".repeat(Math.min(voto0,voto1));
         }
-        System.out.println(selectionString.repeat(voto));
+        System.out.println(selectionString);
     }
 }
